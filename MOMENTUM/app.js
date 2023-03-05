0b3532213595347c27154e6cf36965dcbc69b717
junha kim <tto9011@naver.com>
2023-03-05 16:50:33 +0900

    dd

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,13 +1,3 @@
-
-let a = 7;
-let b = 2;
-
-let myName = "jun";
-
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("hello "+ myName);
-
-myName = "junha";
-console.log("yout name is " + myName);
+const amIFat = null;
+let something;
+console.log(something);
