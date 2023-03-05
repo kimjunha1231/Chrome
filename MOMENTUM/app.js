80a578a3d463de94ea65df2b2f9de85193dc12fe
junha kim <tto9011@naver.com>
2023-03-05 16:59:55 +0900

    ㅇ

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,3 +1,10 @@
-const amIFat = null;
-let something;
-console.log(something);
+const mon ="mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const set = "set";
+const sun = "sund";
+
+const daysOfWeek = mon, tue , wed , thu, fri , set , sun;
+console.log(daysOfWeek);
