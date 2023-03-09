659006078a2241ab26761b76ad278d495597c2c3
kim junha <tto9011@naver.com>
2023-03-09 21:16:21 +0900

    dd

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,13 +1,6 @@
+const a = 5;
+let isNicoFat = true;
 
+isNicoFat =false;
 
-const player = {
-    name: "nico",
-    sayHello: function(otherPersonName){
-        console.log("helo " + otherPersonName + "nice to meet you");
-    },
-};
-
-console.log(player.name);
-player.sayHello("lynn");
-player.sayHello("nico");
-
+console.log(a);
