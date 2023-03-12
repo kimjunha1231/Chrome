3bc6233407c6a0b1c80d4d8a69d473ba78175168
junha kim <tto9011@naver.com>
2023-03-13 00:16:53 +0900

    dd

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,3 +1,9 @@
-const title = document.querySelector(".hello h1");
+const title = document.querySelector("div.hello:first-child h1");
 
-console.log(title);
+function handleTitleClick(){
+    title.style.color="blue";
+}
+
+
+title.addEventListener("click", handleTitleClick);
+
