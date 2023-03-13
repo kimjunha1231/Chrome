f4e8f02efa32d49e4f5f0149f26e2611eb0162b1
junha kim <tto9011@naver.com>
2023-03-13 23:48:20 +0900

    rr

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -4,6 +4,16 @@ function handleTitleClick(){
     title.style.color="blue";
 }
 
+function handleMouseEnter(){
+    title.innerText = "Mouse is here!"
+}
+
+
+function handleMouseLeave(){
+    title.innerText = "Mouse is gone!"
+}
 
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
 
