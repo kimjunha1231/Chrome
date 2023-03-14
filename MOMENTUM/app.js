336dfe1809e5303afedeb52c0d2a311f571a4526
junha kim <tto9011@naver.com>
2023-03-14 13:55:50 +0900

    dd

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,35 +1,15 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    h1.style.color="blue";
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue"){
+        newColor = "tomato";
+    } else{
+        newColor= "blue";
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter(){
-    h1.innerText = "Mouse is here!"
-}
-
-
-function handleMouseLeave(){
-    h1.innerText = "Mouse is gone!"
-}
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy(){
-   alert("copier!");
-}
-function handleWindowOffline(){
-    alert("SOS no WIFI!");
- }
- function handleWindowOnline(){
-    alert("ALL GOOD!");
- }
 
 h1.addEventListener("click",handleTitleClick);
-h1.addEventListener("mouseenter",handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
