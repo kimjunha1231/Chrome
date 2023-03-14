2c5be59035e5d373dd87ae58c94b01b45241db92
junha kim <tto9011@naver.com>
2023-03-14 14:37:29 +0900

    dd

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,12 +1,7 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    const clickedClass = "clicked";
-    if (h1.className === clickedClass) {
-        h1.className = "";
-    } else {
-        h1.className = clickedClass;
-    }
+    h1.classList.toggle("clicked")
 }
 
 
