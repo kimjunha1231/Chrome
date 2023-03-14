5863ea5c8c6c328bc15e00a1ce95936e81f3fd78
junha kim <tto9011@naver.com>
2023-03-14 14:23:58 +0900

    dd

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,15 +1,13 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
-function handleTitleClick(){
-    const currentColor = h1.style.color;
-    let newColor;
-    if(currentColor === "blue"){
-        newColor = "tomato";
-    } else{
-        newColor= "blue";
+function handleTitleClick() {
+    const clickedClass = "clicked";
+    if (h1.className === clickedClass) {
+        h1.className = "";
+    } else {
+        h1.className = clickedClass;
     }
-    h1.style.color = newColor;
 }
 
 
-h1.addEventListener("click",handleTitleClick);
+h1.addEventListener("click", handleTitleClick);
