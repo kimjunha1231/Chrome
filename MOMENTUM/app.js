ffc4a5af7128e4cee551ea1e143603bcf8bcfec4
junha kim <tto9011@naver.com>
2023-03-16 22:55:33 +0900

    ss

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,10 +1,11 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick(){
-    const username = loginInput.value;
-    console.log(username);
+function onLoginSubmit(event){
+    event.preventDefault();
+    console.log(loginInput.value);
 }
 
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+ 
