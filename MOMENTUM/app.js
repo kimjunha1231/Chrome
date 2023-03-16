b24327d8f1e60fdb402df70d3d6b9a4f4e52c8f3
junha kim <tto9011@naver.com>
2023-03-16 21:43:42 +0900

    dd

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,8 +1,10 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleTitleClick() {
-    h1.classList.toggle("clicked")
+function onLoginBtnClick(){
+    console.log(loginInput.value);
+  
 }
 
 
-h1.addEventListener("click", handleTitleClick);
+loginButton.addEventListener("click", onLoginBtnClick);
