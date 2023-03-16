60a6861210acd1f4eb26360a8e8592b31c153116
junha kim <tto9011@naver.com>
2023-03-16 23:28:41 +0900

    ddd

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,18 +1,20 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting  = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
     event.preventDefault();
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+   
+    greeting.innerText = `Hello ${username} Keep going`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick(event){
-    event.preventDefault();
-    console.log(event);
 
-}
+
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick); 
+ 
