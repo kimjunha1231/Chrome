57f56ef0b45abe8c8d5dfc7d8e1f395467841006
junha kim <tto9011@naver.com>
2023-03-17 14:43:59 +0900

    dd

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,20 +1,29 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const greeting  = document.querySelector("#greeting");
+const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-function onLoginSubmit(event){
+function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
+
     loginForm.classList.add(HIDDEN_CLASSNAME);
-   
-    greeting.innerText = `Hello ${username} Keep going`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 
+}
 
-
-loginForm.addEventListener("submit", onLoginSubmit);
- 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}  
