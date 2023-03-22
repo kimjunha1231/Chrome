818101220d0afc30a8072bed1e7112d60337f3c0
junha kim <tto9011@naver.com>
2023-03-22 22:11:39 +0900

    dwq

--- MOMENTUM/js/todo.js
+++ MOMENTUM/js/todo.js
@@ -17,13 +17,14 @@ function deleteToDo(event){
 
 function paintToDo(newTodo){
     const li = document.createElement("li");
+    li.id= newTodo.id;
     const span = document.createElement("span");
     const button = document.createElement("button");
     button.innerText= "❌";
     button.addEventListener("click", deleteToDo);
     li.appendChild(span);
     li.appendChild(button);
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     toDoList.appendChild(li);
 }
 
@@ -31,8 +32,12 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value="";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos(); 
 }
 
